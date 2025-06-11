1d816133f35bcbd66428e434f731b69b69af042b
soeunSim <629943@naver.com>
2025-06-11 22:59:27 +0900

    feat: router 추가, 이슈 리스트 페이지 -> 신규 이슈 등록 페이지로 이동

--- src/pages/IssueList.vue
+++ src/pages/IssueList.vue
@@ -5,7 +5,7 @@
       <IssueListItem v-for="issue in issues" :key="issue.id" :issue="issue" />
     </ul>
     <div>
-      <BaseButton buttonName="신규 이슈 등록" variant="primary" />
+      <BaseButton @click="goToNewIssue" buttonName="신규 이슈 등록" variant="primary" />
     </div>
   </div>
 </template>
@@ -14,7 +14,13 @@
 import IssueListItem from '../components/issue/IssueListItem.vue'
 import { issues as issueMockListData } from '../data/mockData.js'
 import BaseButton from '../components/base/BaseButton.vue'
+import { useRouter } from 'vue-router'
 import { reactive } from 'vue'
 
 const issues = reactive(issueMockListData)
+
+const router = useRouter()
+function goToNewIssue() {
+  router.push('issue/new')
+}
 </script>
