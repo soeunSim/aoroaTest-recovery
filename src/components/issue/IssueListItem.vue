86228e0875d6b3edd3162db4b7547f7ea6474636
soeunSim <629943@naver.com>
2025-06-12 07:09:27 +0900

    feat: 이슈 클릭 시 상세 페이지로 이동 기능 추가

--- src/components/issue/IssueListItem.vue
+++ src/components/issue/IssueListItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <li class="issue-list-item">
+  <li class="issue-list-item" @click="goToDetail(issue.id)">
     <span class="issue-status">{{ props.issue.status }}</span>
     <p class="issue-title">{{ props.issue.title }}</p>
     <div>
@@ -10,7 +10,14 @@
 </template>
 
 <script setup>
+import { useRouter } from 'vue-router'
+
 const props = defineProps({
   issue: Object,
 })
+
+const router = useRouter()
+function goToDetail(id) {
+  router.push(`/issues/${id}`)
+}
 </script>
