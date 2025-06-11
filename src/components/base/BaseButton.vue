b0b94c236ab2b42b044838445e6812409ab4dae7
soeunSim <629943@naver.com>
2025-06-11 22:30:30 +0900

    feat: 공통 스타일 적용 가능한 BaseButton 컴포넌트 추가

---
+++ src/components/base/BaseButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <button :class="['btn', variant]">
+    {{ props.buttonName }}
+  </button>
+</template>
+
+<script setup>
+const props = defineProps({
+  buttonName: String,
+  variant: {
+    type: String,
+    default: 'primary',
+  },
+})
+</script>
--- src/pages/IssueList.vue
+++ src/pages/IssueList.vue
@@ -4,12 +4,16 @@
     <ul>
       <IssueListItem v-for="issue in issues" :key="issue.id" :issue="issue" />
     </ul>
+    <div>
+      <BaseButton buttonName="신규 이슈 등록" variant="primary" />
+    </div>
   </div>
 </template>
 
 <script setup>
 import IssueListItem from '../components/issue/IssueListItem.vue'
 import { issues as issueMockListData } from '../data/mockData.js'
+import BaseButton from '../components/base/BaseButton.vue'
 import { reactive } from 'vue'
 
 const issues = reactive(issueMockListData)
