4d10952a610d52cc33f1b4e8ec89671d2c665329
soeunSim <629943@naver.com>
2025-06-12 02:22:28 +0900

    fix: BaseButton에 type props 추가 및 기본 type='button' 설정

--- src/components/base/BaseButton.vue
+++ src/components/base/BaseButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button :class="['btn', variant]" @click="handleClick">
+  <button :class="['btn', variant]" :type="type" @click="handleClick">
     {{ props.buttonName }}
   </button>
 </template>
@@ -11,11 +11,16 @@ const props = defineProps({
     type: String,
     default: 'primary',
   },
+  type: {
+    type: String,
+    default: 'button',
+  },
 })
 
 const emit = defineEmits(['click'])
 
 function handleClick(event) {
+  event.preventDefault()
   emit('click', event)
 }
 </script>
