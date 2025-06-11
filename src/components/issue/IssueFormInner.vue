1ee28e464502cc128f9d5f485b10786aa4a1cf6a
soeunSim <629943@naver.com>
2025-06-12 02:13:50 +0900

    feat: IssueFormInner 컴포넌트 입력 값 데이터 바인딩 완료

--- src/components/issue/IssueFormInner.vue
+++ src/components/issue/IssueFormInner.vue
@@ -2,23 +2,30 @@
   <form @submit.prevent>
     <div>
       <div>
-        <label for="">제목</label>
-        <input id="" />
+        <label for="formTitle">제목</label>
+        <input id="formTitle" v-model="localForm.title" />
       </div>
       <div>
-        <label for="">설명</label>
-        <textarea id="" />
+        <label for="formDescription">설명</label>
+        <textarea id="formDescription" v-model="localForm.description" />
       </div>
       <div>
-        <label for="">담당자</label>
-        <select id="">
-          <option></option>
+        <label for="formUser">담당자</label>
+        <select id="formUser" v-model="localForm.userId">
+          <option disabled value="">담당자 선택</option>
+          <option v-for="user in props.selectUsers" :key="user.id" :value="user.id">
+            {{ user.name }}
+          </option>
         </select>
       </div>
       <div>
-        <label for="">상태</label>
-        <select id="">
-          <option></option>
+        <label for="formStatus">상태</label>
+        <select id="formStatus" v-model="localForm.status">
+          <option disabled value="">상태 선택</option>
+          <option value="PENDING">PENDING</option>
+          <option value="IN_PROGRESS">IN_PROGRESS</option>
+          <option value="COMPLETED">COMPLETED</option>
+          <option value="CANCELLED">CANCELLED</option>
         </select>
       </div>
       <div>
@@ -31,8 +38,16 @@
 
 <script setup>
 import BaseButton from '../base/BaseButton.vue'
+import { reactive } from 'vue'
+
+const props = defineProps({
+  formData: Object,
+  selectUsers: Object,
+})
 
 const emit = defineEmits(['goBack'])
+
+const localForm = reactive({ ...props.formData })
 </script>
 
 <style scoped></style>
--- src/pages/IssueForm.vue
+++ src/pages/IssueForm.vue
@@ -1,12 +1,31 @@
 <template>
   <div>
-    <IssueFormInner @goBack="goToBack"></IssueFormInner>
+    <IssueFormInner
+      :formData="formData"
+      :selectUsers="selectUsers"
+      @goBack="goToBack"
+    ></IssueFormInner>
   </div>
 </template>
 
 <script setup>
 import IssueFormInner from '../components/issue/IssueFormInner.vue'
+import { ref, reactive } from 'vue'
 import { useRouter } from 'vue-router'
+import { users, issues } from '../data/mockData'
+
+const formData = reactive({
+  title: '',
+  description: '',
+  status: 'PENDING',
+  userId: '',
+  user: null,
+  createdAt: new Date().toISOString(),
+  updatedAt: new Date().toISOString(),
+})
+
+const selectUsers = reactive(users)
+const issueList = ref(issues)
 
 const router = useRouter()
 function goToBack() {
